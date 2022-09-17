73c15906cc6b8da16dd435df9e66d4669eb3bc4e
Harang Ju <harangju@icloud.com>
2022-09-16 20:51:33 -0400

    use random salts

--- src/utils/UtilCrypto.js
+++ src/utils/UtilCrypto.js
@@ -1,35 +1,42 @@
 import forge from "node-forge";
 
-// generate a random key and IV
-// Note: a key size of 16 bytes will use AES-128, 24 => AES-192, 32 => AES-256
-// let key = forge.random.getBytesSync(16);
-// let iv = forge.random.getBytesSync(16);
+const NUM_ITERATIONS = 100;
+const BYTES_KEY = 16;
+const BYTES_SALT = 64;
+const PREFIX_SALT = 'Salted__'; // (add to match openssl tool output)
 let iv = '\x87{\x89Þ\fª¹Õ²­&Wxv\x10õ';
 
-// alternatively, generate a password-based 16-byte key
-// let salt = forge.random.getBytesSync(128);
-let salt = 'Á¤\x90\x92[A\x8Bvâdh\\Ëk·\x81';
-let numIterations = 100;
-let key = forge.pkcs5.pbkdf2('password', salt, numIterations, 16);
-
-function encrypt(text) {
+function encrypt(text, password) {
+  let salt = forge.random.getBytesSync(BYTES_SALT);
+  console.log('salt', salt);
+  let key = forge.pkcs5.pbkdf2(password, salt, NUM_ITERATIONS, BYTES_KEY);
   let cipher = forge.cipher.createCipher("AES-CBC", key);
   cipher.start({ iv: iv });
   cipher.update(forge.util.createBuffer(text));
   cipher.finish();
-  return cipher.output.data;
+  console.log('encrypted: ', cipher.output.data);
+  var output = forge.util.createBuffer();
+  output.putBytes(PREFIX_SALT);
+  output.putBytes(salt);
+  output.putBuffer(cipher.output);
+  return output.getBytes();
 }
 
-function decrypt(bytes) {
+function decrypt(bytes, password) {
+  let input = forge.util.createBuffer(bytes, 'binary');
+  input.getBytes(PREFIX_SALT.length);
+  let salt = input.getBytes(BYTES_SALT);
+  console.log('salt: ', salt);
+  let key = forge.pkcs5.pbkdf2(password, salt, NUM_ITERATIONS, BYTES_KEY);
   let decipher = forge.cipher.createDecipher("AES-CBC", key);
   decipher.start({ iv: iv });
-  // decipher.update(bytes);
-  decipher.update(forge.util.createBuffer(bytes));
+  decipher.update(input);
+  // decipher.update(forge.util.createBuffer(input));
   let success = decipher.finish();
   if (success) {
     return decipher.output.data;
   } else {
-    return decipher.output.data;
+    return null;
   }
 }
 
