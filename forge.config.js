258c7eea4013b49a683bd7baf5d4afbb790d3690
Harang Ju <harangju@icloud.com>
2022-12-14 14:58:22 -0500

    package for mac

---
+++ forge.config.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+module.exports = {
+  packagerConfig: {
+    icon: 'src/assets/icon',
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    },
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
+
+console.log(process.env.APPLE_ID);
